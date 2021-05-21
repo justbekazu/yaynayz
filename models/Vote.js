3c2e66f36a0b52ef348b3578e7b4f90a8ea3ad7c
Luis Cabrera <54341829+luiscabrera77@users.noreply.github.com>
2021-05-21 14:33:02 -0500

    Update Vote.js
    Changed Datatypes, added auto-increments and relationship with questions, named model "Vote"

--- models/Vote.js
+++ models/Vote.js
@@ -7,13 +7,21 @@ class Vote extends Model {}
 
 Vote.init({
     id: {
-        type: DataTypes.BOOLEAN,
-        primaryKey: true
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
+    question_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'question',
+        key: 'id'
+      }
 }, {
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'vote'
 });
-module.exports = Comment;
+module.exports = Vote;
